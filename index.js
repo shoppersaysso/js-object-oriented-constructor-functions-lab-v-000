484126bf2032936c91b013859c095ece29c4461b
Sara Hopper <sara.hopper@gmail.com>
2017-09-28 00:43:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function Scooter(name, age, hometown) {
-  this.name = name
-  this.age = age
-  this.hometown = hometown
+function Scooter(year, color, model) {
+  this.year = year
+  this.color = color
+  this.model = model
 }
  
